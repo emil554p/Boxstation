12cff79d0f171ff722b6101e7018c637e4e25ce2
Emil <emilkr11@gmail.com>
2020-02-13 08:07:44 +0100

    Boxstation navbar and elements on front page created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,146 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3 mb-5">
+          <a class="h2 navbar-brand" href="#">Boxstation</a>
+
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav d-flex">
+                  <li class="nav-item align-self-center active">
+                      <a class="nav-link h4 pr-5" href="#">Betaling<span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item align-self-center ">
+                      <a class="nav-link h4 pr-5" href="#">Kapacitet</a>
+                  </li>
+                  <li class="nav-item align-self-center ">
+                      <a class="nav-link h4 pr-5" href="#">Kunder</a>
+                  </li>
+                  <li>
+                      <a class="nav-link h1 pr-5"><i class="nav item far fa-user text-light"></i></a>
+                  </li>
+                  <li>
+                      <a class="nav-link h1 pr-5"><i class="nav item fas fa-search text-light"></i></a>
+                  </li>
+
+
+              </ul>
+          </div>
+      </nav>
+
+      <div class="row p-5">
+          <ul class="nav nav-tabs mt-5 col-8">
+              <li class="nav-item">
+                  <a class="nav-link bg-dark text-light" href="#">Nordhavn</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link active" href="#">Herlev</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link bg-dark text-light" href="#">Odense</a>
+              </li>
+          </ul>
+
+          <div class="col-4 flex-column">
+                  <div class="card mb-5 ">
+                      <div class="card-header bg-dark text-light">
+                          Statistik - Odense
+                      </div>
+                      <div class="row">
+                      <ul class="list-group list-group-flush col-5">
+                          <li class="list-group-item border-0">Udlejet</li>
+                          <li class="list-group-item border-0">Reserveret</li>
+                          <li class="list-group-item border-0">Opsagte</li>
+                          <li class="list-group-item border-0">Ledige</li>
+                      </ul>
+                          <ul class="list-group list-group-flush col-4">
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"> </li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                          </ul>
+                          <ul class="list-group list-group-flush col-3">
+                              <li class="list-group-item border-0">60%</li>
+                              <li class="list-group-item border-0">10%</li>
+                              <li class="list-group-item border-0">15%</li>
+                              <li class="list-group-item border-0">15%</li>
+                          </ul>
+                     </div>
+                  </div>
+                  <div class="card">
+                      <div class="card-header bg-dark text-light">
+                          Samlet statistik
+                      </div>
+                      <div class="row">
+                          <ul class="list-group list-group-flush col-5">
+                              <li class="list-group-item border-0">Udlejet</li>
+                              <li class="list-group-item border-0">Reserveret</li>
+                              <li class="list-group-item border-0">Opsagte</li>
+                              <li class="list-group-item border-0">Ledige</li>
+                          </ul>
+                          <ul class="list-group list-group-flush col-4">
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                              <li class="list-group-item border-0 w-100 bg-dark mt-3"></li>
+                          </ul>
+                          <ul class="list-group list-group-flush col-3">
+                              <li class="list-group-item border-0">60%</li>
+                              <li class="list-group-item border-0">10%</li>
+                              <li class="list-group-item border-0">15%</li>
+                              <li class="list-group-item border-0">15%</li>
+                          </ul>
+                      </div>
+                  </div>
+          </div>
+      </div>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  data: []
+}).$mount('#app');
